c00aa01eae40711d93a4db770756af18c5f8e73b
Akmalkhon Burkhonov <akmalburhonov5515@gmail.com>
2025-06-15 12:23:37 +0500

    Initial Commit

---
+++ TemplateMVVM/TemplateMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemplateMVVMApp.swift
+//  TemplateMVVM
+//
+//  Created by itserviceimac on 15/06/25.
+//
+
+import SwiftUI
+
+@main
+struct TemplateMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
